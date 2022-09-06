0e3f67795ebda7face69a16b480620d4e97e591f
KirilichevAO <kirilichev.a@icloud.com>
2022-09-06 11:20:03 +0700

    58

---
+++ 58/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secondMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secondMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] firstArray2D = CreateMatrixRndInt(3, 3, 0, 9);
+PrintMatrix(firstArray2D);
+Console.WriteLine();
+
+int[,] secondArray2D = CreateMatrixRndInt(3, 3, 0, 9);
+PrintMatrix(secondArray2D);
+Console.WriteLine();
+
+int[,] resultArray2D = new int[3, 3];
+MultiplyMatrix(firstArray2D, secondArray2D, resultArray2D);
+PrintMatrix(resultArray2D);
